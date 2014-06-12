ad320d0faa2309f89db33f6ad07c650b4f962266
Hagane <deemson@gmail.com>
2014-06-13 01:53:49 +0600

    Now the ships can be controlled separately and this thing looks similar to an actual game. #1 fixed

--- Assets/Code/InputMapping.cs
+++ Assets/Code/InputMapping.cs
@@ -3,8 +3,7 @@ using System.Collections;
 
 public class InputMapping : MonoBehaviour {
 	public delegate void InputAction();
-
-
+	
 	public KeyCode rotateCCW;
 	public KeyCode rotateCW;
 	public KeyCode accelerate;
@@ -14,5 +13,28 @@ public class InputMapping : MonoBehaviour {
 	public event InputAction OnInputRotateCW;
 	public event InputAction OnInputAccelerate;
 	public event InputAction OnInputFire;
+
+	public void Update()
+	{
+		if(Input.GetKey(rotateCCW) && OnInputRotateCCW != null)
+		{
+			OnInputRotateCCW();
+		}
+
+		if(Input.GetKey(rotateCW) && OnInputRotateCW != null)
+		{
+			OnInputRotateCW();
+        }
+
+		if(Input.GetKey(accelerate) && OnInputAccelerate != null)
+		{
+			OnInputAccelerate();
+        }
+
+		if(Input.GetKey(fire) && OnInputFire != null)
+		{
+			OnInputFire();
+        }
+	}
 }
 
